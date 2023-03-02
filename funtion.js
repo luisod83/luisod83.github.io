bfd2b0dd7cb910ace3c94e467a9701d00b3e4623
luisod83 <126726667+luisod83@users.noreply.github.com>
2023-03-01 21:33:15 -0300

    Add files via upload

---
+++ funtion.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById('boton').onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Lenguajes de programación, scripting y markup, Control de código de fuente, Fundamentos de testing (pruebas de software).Entornos de desarrollo integrados (IDE).Editores de texto. Conceptos básicos de la red.Estructuras de datos y algoritmos. Base de datos.Sistemas operativos. Cifrado y criptografía.Ciclo de vida del desarrollo de software (SDLC).";
+}
